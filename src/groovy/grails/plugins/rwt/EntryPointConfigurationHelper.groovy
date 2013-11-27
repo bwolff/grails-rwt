4b306b5dbabed59ba10323c711a4b485f3189587
Benjamin Wolff <benjamin.wolff@cern.ch>
2013-11-27 21:24:12 +0100

    Issue #7: Invalid setting for custom theme definition described in documentation

--- src/groovy/grails/plugins/rwt/EntryPointConfigurationHelper.groovy
+++ src/groovy/grails/plugins/rwt/EntryPointConfigurationHelper.groovy
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * Copyright 2012-2013 Benjamin Wolff
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * Contributors:
  *     Benjamin Wolff - initial API and implementation
  ******************************************************************************/
@@ -54,7 +54,7 @@ class EntryPointConfigurationHelper {
                         " entry point configuration: ${name}")
             }
 
-            String themeId = values.theme ?: null
+            String themeId = values.themeId ?: null
             if (themeId) {
                 themeId = themeId.toLowerCase() // Ignore capitalization.
             }
